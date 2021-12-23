5a44e61ffd30d2bab6656f7311f6f2eba2bb093e
KS_HTK <HKoertge@gmx.de>
2021-12-23 07:42:09 +0100

    solved day23 without code

---
+++ day23/main.ts
@@ -0,0 +1,22 @@
+//AdventOfCode Day 23
+
+/*
+Todays solution comes from the paper.
+Solved by hand. So no code here.
+
+#############
+#...........#
+###B#B#C#D###
+  #D#C#A#A#
+  #########
+Cost: 15111
+
+#############
+#...........#
+###B#B#C#D###
+  #D#C#B#A#
+  #D#B#A#C#
+  #D#C#A#A#
+  #########
+Cost: 47625
+*/
